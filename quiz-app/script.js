83aa90d041967b26552d27ed70b7d3236b26169b
Arnav Sharma <arnav1776@gmail.com>
2021-08-26 11:33:36 +0530

    quiz-app

---
+++ quiz-app/script.js
@@ -0,0 +1,24 @@
+const quizData = [
+    {
+        question: 'What is the capital of India?',
+        a: 'Mumbai',
+        b: 'New Delhi',
+        c: 'Kolkata',
+        d: 'Chennai',
+        correct: 'b'
+    },{
+        question: 'Who was the first woman Prime Minister of India?',
+        a: 'Sarojini Naidu',
+        b: 'Parthibha Patil',
+        c: 'Indira Gandhi',
+        d: 'Sonia Gandhi',
+        correct: 'c'
+    },{
+        question: 'What is the National Animal of India?',
+        a: 'Tiger',
+        b: 'Lion',
+        c: 'Elephant',
+        d: 'Cow',
+        correct: 'a'
+    }
+]
